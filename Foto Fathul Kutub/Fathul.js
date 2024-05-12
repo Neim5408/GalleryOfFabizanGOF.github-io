3acf5effad9719ec7e4c713b5a841765bf47faff
Gallery Of Fabizan <176485@daarululuumlido.com>
2024-05-12 12:03:15 +0700

    first commit

---
+++ Foto Fathul Kutub/Fathul.js
@@ -0,0 +1,13 @@
+//   scroll reveal
+ScrollReveal({
+    //   reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+  });
+  
+  ScrollReveal().reveal('.homet-content, .home-content, .heading', { origin: 'top' });
+  ScrollReveal().reveal('.homet-content a, .vid-btn, .home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+  ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+  ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+  
